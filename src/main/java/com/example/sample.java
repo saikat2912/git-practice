7955ac39e9935837ad6d6511b78fbc04492f3f1c
Maniket Kumar <sonuanand644@gmail.com>
2025-03-08 18:05:50 +0530

    added some new code in sample.java

--- src/main/java/com/example/sample.java
+++ src/main/java/com/example/sample.java
@@ -1,6 +1,24 @@
+package com.example;
+
 public class sample
 {
-    public static void main (String args []){
-        int a;
+        public static void main(String[] args) {
+            int arr[] = {2,1,3,5};
+            System.out.println(SumofAllSubarray(arr));
+        }
+        public static int SumofAllSubarray(int arr[])
+        {
+            int ans=0;
+            int n=arr.length;
+            for(int i=0;i<n;i++)
+            {
+                int sum=0;
+                for(int j=i;j<n;j++)
+                {
+                    sum=sum+arr[j];
+                    ans=ans+sum;
+                }
+            }
+            return ans;
     }
 }
