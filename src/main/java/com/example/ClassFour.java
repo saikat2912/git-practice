8db3e6b40611479890642c902465ef860b5903c3
saikat2912 <saikat2270@gmail.com>
2025-03-10 10:53:19 +0530

    new branch aded

---
+++ src/main/java/com/example/ClassFour.java
@@ -0,0 +1,8 @@
+class ClassFour{
+
+    public static void main(String args[]){
+        int a=10;
+        int b=10;
+        System.out.println("The value of a is "+a+" the value of b is "+b);
+    }
+}
