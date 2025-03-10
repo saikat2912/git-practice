1af5f9a345371ad389e0d750c78f4a50712b4e3e
saikat2912 <saikat2270@gmail.com>
2025-03-10 11:43:25 +0530

    main has new changes

--- src/main/java/com/example/ClassFour.java
+++ src/main/java/com/example/ClassFour.java
@@ -3,6 +3,7 @@ class ClassFour{
     public static void main(String args[]){
         int a=10;
         int b=10;
-        System.out.println("The value of a is "+a+" the value of b is "+b);
+        int c=30;
+        System.out.println("The value of a is "+a+" the value of b is "+b+" c is added "+c);
     }
 }
